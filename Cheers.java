94ccf4adb489e914e676ab55df689b835b0d4556
Noam Franchi <noam@Noams-MacBook-Air.local>
2025-11-12 15:32:46 +0200

    done

--- Cheers.java
+++ Cheers.java
@@ -8,6 +8,19 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	   String word = args[0];
+           int timesToRepeat = Integer.parseInt(args[1]);
+           String specialCase = "AEFHILMNORSX";
+           for(int i = 0; i < word.length(); i++){
+                char letter = word.charAt(i);
+                String prefix;
+                if(specialCase.charAt(letter) != -1){
+                        prefix = "an";
+                }else{
+                        prefix = "a ";
+                }
+                System.out.println("Give me " + letter + ": " + letter + " ! ! !");
+           }
+
         }
 }
